dd90fcbcc87e62e343de581184f133a47d74ae9d
MargaritaSv <margarita.esti@gmail.com>
2016-01-07 14:25:20 +0200

    Take wanted  direction from user.

--- Snake_Console/Snake.cs
+++ Snake_Console/Snake.cs
@@ -10,18 +10,48 @@ namespace Snake_Console
     {
         static void Main()
         {
+
+            Position[] directions = new Position[]
+                {
+                    new Position(0,1),//right
+                    new Position(0,-1),//left
+                    new Position(1,0),//down
+                    new Position(-1,0)//up
+                };
+
+
             Queue<Position> snakeElements = new Queue<Position>();
 
             for (int i = 0; i <= 5; i++)
             {
                 snakeElements.Enqueue(new Position(0, i));
-
             }
 
+            int currDirection = 0;
 
             while (true)
             {
-                Console.Clear();
+                ConsoleKeyInfo userInput = Console.ReadKey();
+
+                if (userInput.Key == ConsoleKey.RightArrow)
+                {
+                    currDirection = 0;
+                }
+
+                if (userInput.Key == ConsoleKey.LeftArrow)
+                {
+                    currDirection = 1;
+                }
+
+                if (userInput.Key == ConsoleKey.DownArrow)
+                {
+                    currDirection = 2;
+                }
+
+                if (userInput.Key == ConsoleKey.UpArrow)
+                {
+                    currDirection = 3;
+                }
             }
         }
     }
